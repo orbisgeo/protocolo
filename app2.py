2170090511ab3774f04b895726931e38e677326d
orbisgeo <137726099+orbisgeo@users.noreply.github.com>
2025-02-18 17:57:01 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -70,5 +70,5 @@ def download_file(filename):
         return "Arquivo não encontrado", 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
 
